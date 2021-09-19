4f3739bc9b892422332cddbf1b71faa282093f79
sureshthakur92k <suresh.thakur92k@gmail.com>
2021-09-19 15:32:09 +0530

    Registration
    Registration

--- src/app/Pages/registraion/registraion.page.ts
+++ src/app/Pages/registraion/registraion.page.ts
@@ -32,14 +32,65 @@ RegistraionForm=new FormGroup({
   Address2:new FormControl(''),
   State:new FormControl(''),
   City:new FormControl(''),
+  Block:new FormControl(''),
+  
   
 });
 // RegistraionForm: FormGroup;
+RegObje={
+  FirstName:"",
+  LastName:"",
+  FatherName:"",
+  GrandFatherName:"",
+  Address1:"",
+  Address2:"",
+  StateName:"",
+  cityname:"",
+  blockname:""
+}
+ResetAll()
+{
+ this.RegistraionForm.reset();
+}
 submitRegistationForm() {
   debugger;
-  console.log(this.RegistraionForm.value);
+  // console.log(this.RegistraionForm.value);
+  // console.log(this.AllState[this.RegistraionForm.value.State-1].StateName);
+  // console.log(this.AllCityByState[this.RegistraionForm.value.City]);
+
+var City=this.AllCityByState.filter(
+  City => City.cityid === Number(this.RegistraionForm.value.City));
+
+  var Block=this.AllBlockByCity.filter(
+    block => block.blockid === Number(this.RegistraionForm.value.Block));
+
+  
+
+  this.RegObje.FirstName=this.RegistraionForm.value.FirstName;
+  this.RegObje.LastName=this.RegistraionForm.value.LastName;
+  this.RegObje.FatherName=this.RegistraionForm.value.FatherName;
+  this.RegObje.GrandFatherName=this.RegistraionForm.value.GrandFatherName;
+  this.RegObje.Address1=this.RegistraionForm.value.Address1;
+  this.RegObje.Address2=this.RegistraionForm.value.Address2;
+  this.RegObje.StateName=this.AllState[this.RegistraionForm.value.State-1].StateName;
+  this.RegObje.cityname=City[0].cityname;
+  this.RegObje.blockname=Block[0].blockname
+ // debugger;
+  this.SaveRegistrationService(this.RegObje).subscribe(d=>{
+    var ss=d;
+  });
+ // console.log (this.RegObje);
+  
+
+}
+
+ionViewWillEnter()
+{
+ // debugger;
+  this.ResetAll();
 }
   ngOnInit() {
+    
     this.GetAllStateService().subscribe(data=>{
      // console.log("GOT DATA");
      // console.log(data);
@@ -60,6 +111,23 @@ submitRegistationForm() {
                    "StateId":StateId
               }   
     return this.http.post("http://localhost:4506/api/GetCityByState",postData);
+  }
+
+  SaveRegistrationService(RegObj :any)
+  {
+    var obj={
+      "FirstName":RegObj.FirstName,
+      "LastName":RegObj.LastName,
+      "FatherName":RegObj.FatherName,
+      "GrandFatherName":RegObj.GrandFatherName,
+      "Address1":RegObj.Address1,
+      "Address2":RegObj.Address2,
+      "State":RegObj.StateName,
+      "City":RegObj.cityname,
+      "Block":RegObj.blockname
+      
+    } 
+    return this.http.post("http://localhost:4506/api/NewRegistraion",obj);
   }
   GetAllBlockByCityService(CityId :any)
   {
